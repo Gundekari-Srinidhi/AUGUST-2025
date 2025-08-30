20a5975537108b963d3e91d5033e344817c32751
Gundekari-Srinidhi <gundekarisrinidhi@gmail.com>
2025-08-30 21:58:42 +0530

    Time: 4 ms (5.42%), Space: 17.6 MB (91%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.py
+++ 2507-number-of-common-factors/2507-number-of-common-factors.py
@@ -1,11 +1,11 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        l=[]
+        l=0
         i=1
         max1=min(a,b)
         while(i<=max1):
             if a%i==0 and b%i==0:
-                l.append(i)
+                l+=1
             i+=1
-        return len(l)
+        return l
         
